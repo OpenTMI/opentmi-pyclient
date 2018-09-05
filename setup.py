76e2f8b9841abc3249077afd1c1af6e229dc71a3
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-09-05 22:29:30 +0300

    pylint fix

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class VerifyVersionCommand(install):
     def run(self):
         tag = os.getenv('CIRCLE_TAG')
         if tag != VERSION:
-            info = "Git tag: {0} does not match the"
+            info = "Git tag: {0} does not match the"\
             "version of this app: {1}".format(tag, VERSION)
             sys.exit(info)
 
