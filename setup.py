07ee70e1d5db39a1929f8bddbfd1d1ca2ae65c09
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-02-26 14:56:46 +0200

    update version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools.command.install import install
 DESCRIPTION = "opentmi-client"
 OWNER_NAMES = 'Jussi Vatjus-Anttila'
 OWNER_EMAILS = 'jussiva@gmail.com'
-VERSION = '0.7.0'
+VERSION = '0.8.0'
 
 
 # Utility function to cat in a file (used for the README)
