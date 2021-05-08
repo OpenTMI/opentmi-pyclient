3a71d984eb9838ad498989293f1311ba8d976e63
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-05-08 21:05:22 +0300

    supports for python 3.7,3.8,3.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools.command.install import install
 DESCRIPTION = "opentmi-client"
 OWNER_NAMES = 'Jussi Vatjus-Anttila'
 OWNER_EMAILS = 'jussiva@gmail.com'
-VERSION = '0.9.0'
+VERSION = '0.10.0'
 
 
 # Utility function to cat in a file (used for the README)
