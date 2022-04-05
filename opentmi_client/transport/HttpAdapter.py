b807998d3b91dbe48ef0c48b31baaf0dfd14c28f
Jussi Vatjus-Anttila <jussiva@gmail.com>
2022-04-05 13:45:44 +0300

    add missing docstrings

--- opentmi_client/transport/HttpAdapter.py
+++ opentmi_client/transport/HttpAdapter.py
@@ -1,20 +1,33 @@
+"""
+HTTPAdapter
+"""
+import urllib.parse
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
-import urllib.parse
+
 
 DEFAULT_TIMEOUT = 30  # seconds
 
 
 class TimeoutHTTPAdapter(HTTPAdapter):
+    """
+    Timeout adapter that pass default timeout
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Constructor for TimeoutHTTPAdapter
+        :param args: HttpAdapter args
+        :param kwargs: HTTPAdapter kwargs
+        """
         self.timeout = DEFAULT_TIMEOUT
         if "timeout" in kwargs:
             self.timeout = kwargs["timeout"]
             del kwargs["timeout"]
         super().__init__(*args, **kwargs)
 
-    def send(self, request, **kwargs):
+    def send(self, request, **kwargs):  # pylint: disable=arguments-differ
+        """ HTTPAdapter send method """
         timeout = kwargs.get("timeout")
         if timeout is None:
             kwargs["timeout"] = self.timeout
@@ -23,12 +36,12 @@ class TimeoutHTTPAdapter(HTTPAdapter):
 
 def create_http_session(base_url: str) -> requests.Session:
     """
+    Create requests session
     @example
     > s = create_http_session('http://localhost', 'mytoken')
     > s.get(s.base_url_join('/api'))
-    :param base_url:
-    :param token:
-    :return:
+    :param base_url: base url
+    :return: Session instance
     """
     retry_strategy = Retry(
         total=3,
