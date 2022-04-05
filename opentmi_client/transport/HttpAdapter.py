c9ef69a76fb05147823c5cd18028dbc18aac2471
Jussi Vatjus-Anttila <jussiva@gmail.com>
2022-04-05 13:50:22 +0300

    import Retry from urllib3

--- opentmi_client/transport/HttpAdapter.py
+++ opentmi_client/transport/HttpAdapter.py
@@ -2,9 +2,9 @@
 HTTPAdapter
 """
 import urllib.parse
+from urllib3.util.retry import Retry
 import requests
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
 
 
 DEFAULT_TIMEOUT = 30  # seconds
