30da603b369d13064c91e30b45731d02da7ea3ce
Jussi Vatjus-Anttila <jussiva@gmail.com>
2024-10-05 14:21:57 +0300

    prep next rel

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools.command.install import install
 DESCRIPTION = "opentmi-client"
 OWNER_NAMES = 'Jussi Vatjus-Anttila'
 OWNER_EMAILS = 'jussiva@gmail.com'
-VERSION = '0.10.3'
+VERSION = '0.11.0'
 
 
 # Utility function to cat in a file (used for the README)
