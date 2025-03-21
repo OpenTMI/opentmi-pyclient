9cd2a0cb9374956a6022874f5625a1f651a5e79f
Jussi Vatjus-Anttila <jussiva@gmail.com>
2025-03-21 21:57:52 +0200

    fix import
    Signed-off-by: Jussi Vatjus-Anttila <jussiva@gmail.com>

--- test/__init__.py
+++ test/__init__.py
@@ -4,7 +4,6 @@ from .test_api_build import TestBuild
 from .test_api_result import TestResult
 from .test_tools import TestTools
 from .test_base import TestBase
-from .test_logger import TestLogger
 from .test_query import TestRequest
 from .test_transport import TestRequest
 from .test_cli import TestCli
